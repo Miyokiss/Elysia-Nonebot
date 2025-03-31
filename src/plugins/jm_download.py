fdf7410881f2cd85c2f7204b2aaa98703e4825d9
SlyAimer <2289782085@qq.com>
2025-03-31 18:08:57 +0800

    fix(jm_download): 添加下载提示和空输入校验
    在下载前发送等待提示，并检查输入是否为空

--- src/plugins/jm_download.py
+++ src/plugins/jm_download.py
@@ -8,7 +8,11 @@ from src.clover_image.delete_file import delete_file
 jm = on_command("jm", rule=to_me(), priority=10,block=False)
 @jm.handle()
 async def handle_function(message: MessageEvent):
+    await jm.send("正在下载中，请稍等")
     key = message.get_plaintext().replace("/jm", "").strip(" ")
+
+    if key == "":
+        await jm.finish("请输入jm的id")
 
     output_file = await download_jm(key)
     await jm.send(MessageSegment.file_image(Path(output_file)))
