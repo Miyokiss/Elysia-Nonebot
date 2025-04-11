231f76731951820fb98684c26e897486019e6036
SlyAimer <2289782085@qq.com>
2025-04-11 09:57:50 +0800

    fix(daily_report): 添加日报获取错误处理及日志记录

--- src/plugins/daily_report.py
+++ src/plugins/daily_report.py
@@ -5,6 +5,7 @@ from nonebot.plugin import on_command
 from nonebot.adapters.qq import  MessageSegment
 from src.clover_report.data_source import Report
 from src.configs.path_config import temp_path
+from nonebot import logger
 import os
 
 
@@ -15,5 +16,9 @@ async def handle_function():
     file = Path() / temp_path / f"{now.date()}日报.png"
     if not os.path.exists(file):
         await daily_report.send("您是今天第一个查看日报的哦，来看看世界上都发生了些什么吧💫\nCrunching the latest news, just for you. Hang tight…")
-    await Report.get_report_image()
+    try:
+        await Report.get_report_image()
+    except Exception as e:
+        logger.error(f"日报获取失败：{e}")
+        await daily_report.finish("出错啦，请重试。")
     await daily_report.finish(MessageSegment.file_image(Path(temp_path+f"{now.date()}日报.png")))
