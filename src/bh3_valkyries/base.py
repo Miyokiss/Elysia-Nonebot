49985ffb74f61a6bd64b3b7b17d544ea079856a1
Miyokiss <Miyokiss@miyo.wiki>
2025-06-14 08:59:18 +0800

    ADD:bh3_valkyries

---
+++ src/bh3_valkyries/base.py
@@ -0,0 +1,24 @@
+from os import getcwd
+from nonebot.log import logger
+from playwright.async_api import async_playwright
+from nonebot_plugin_htmlrender import template_to_pic
+from src.clover_music.cloud_music.data_base import save_img
+
+__name__ = "bh3_valkyries | base"
+
+async def all_valkyrie_info_img(data, temp_file):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+    image_bytes = await template_to_pic(
+        template_path=getcwd() + "/src/clover_html/BH3",
+        template_name="VALKYRIES.html",
+        templates={"data": data},
+        pages={
+            "viewport": {"width": 580, "height": 1},
+            "base_url": f"file://{getcwd()}",
+        },
+        wait=2,
+    )
+    await save_img(image_bytes,temp_file)
+    await browser.close()
+    return True
