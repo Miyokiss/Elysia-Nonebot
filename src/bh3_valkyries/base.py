f20a1e92e12804c31c39ca8e79ec85ae254c82f3
Miyokiss <Miyokiss@miyo.wiki>
2025-06-19 00:39:36 +0800

    feat(bh3_valkyries): 优化角色信息展示并添加详情页面
    - 修正角色列表信息图片生成逻辑
    - 新增单个角色详细信息图片生成功能
    - 优化用户指令处理，增加错误提示
    - 调整角色搜索和结果显示方式

--- src/bh3_valkyries/base.py
+++ src/bh3_valkyries/base.py
@@ -6,9 +6,9 @@ from src.clover_music.cloud_music.data_base import save_img
 
 __name__ = "bh3_valkyries | base"
 
-async def valkyrie_info_img(data, temp_file, title):
+async def valkyries_info_img(data, temp_file, title):
     """
-    获取角色获取信息图片
+    获取角色列表信息图片
     """
     async with async_playwright() as p:
         browser = await p.chromium.launch()
@@ -19,6 +19,26 @@ async def valkyrie_info_img(data, temp_file, title):
                    "title": title},
         pages={
             "viewport": {"width": 580, "height": 1},
+            "base_url": f"file://{getcwd()}",
+        },
+        wait=2,
+    )
+    await save_img(image_bytes,temp_file)
+    await browser.close()
+    return True
+
+async def valkyrie_info_img(data, temp_file):
+    """
+    获取角色信息图片
+    """
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+    image_bytes = await template_to_pic(
+        template_path=getcwd() + "/src/clover_html/BH3",
+        template_name="valkyrie.html",
+        templates={"data": data},
+        pages={
+            "viewport": {"width": 1185, "height": 1},
             "base_url": f"file://{getcwd()}",
         },
         wait=2,
