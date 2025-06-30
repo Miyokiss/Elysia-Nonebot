da1d8a69a6786dcd91bd6dd9b64229f502fda51e
Miyokiss <Miyokiss@miyo.wiki>
2025-06-30 22:45:55 +0800

    Refactoring(fortune)：重构fortune

--- src/plugins/fortune.py
+++ src/plugins/fortune.py
@@ -1,41 +1,65 @@
-# 引入sqlalchemy依赖
+import os
+import time
+from os import getcwd
 from pathlib import Path
+from nonebot import logger
 from nonebot.rule import to_me
 from nonebot.plugin import on_command
+from nonebot.utils import logger_wrapper
+from nonebot.exception import FinishedException
+from playwright.async_api import async_playwright
+from nonebot_plugin_htmlrender import template_to_pic
+from nonebot.adapters.qq import Message, MessageEvent, MessageSegment
 from src.configs.Keyboard_config import Keyboard_fortune
-from nonebot.adapters.qq import Message, MessageEvent, MessageSegment, exception
 from nonebot.adapters.qq.message import MessageKeyboard
-from src.clover_image.get_image import get_image_names
 from src.clover_sqlite.models.fortune import QrFortune
 from src.clover_sqlite.models.tarot import TarotExtractLog
-import time
+from src.clover_music.cloud_music.data_base import save_img
+from src.clover_image.delete_file import delete_file
+from src.configs.path_config import temp_path
+
+logger_wrapper("fortune")
 
 fortune_by_sqlite = on_command("今日运势", rule=to_me(), priority=10)
 @fortune_by_sqlite.handle()
 async def get_today_fortune(message: MessageEvent):
-
-    local_image_path = await get_image_names()
-    result = await QrFortune.get_fortune(message.get_user_id())
-
-    content = ("\n" + "您的今日运势为：" + "\n" +
-               result.fortune_summary + "\n" +
-               result.lucky_star + "\n" +
-               "签文：" + result.sign_text + "\n" +
-               "————————————" + "\n" +
-               "解签：" + result.un_sign_text)
-
-    msg = Message([
-        MessageSegment.file_image(Path(local_image_path)),
-        MessageSegment.text(content),
-    ])
     try:
-        await fortune_by_sqlite.send(msg)
+        user_id = message.get_user_id()
+        result = await QrFortune.get_fortune(user_id)
+        if result is None:
+            logger.error("今日运势获取失败")
+            await fortune_by_sqlite.finish("今日运势获取失败")
+        temp_file = os.path.join(temp_path, f"{user_id}_{time.time()}.png")
+        data = {
+             "fortune_summary": result.fortune_summary,
+             "lucky_star": result.lucky_star,
+             "sign_text": result.sign_text,
+             "un_sign_text": result.un_sign_text
+        }
+        logger.debug(f"data：{data}")
+        async with async_playwright() as p:
+            browser = await p.chromium.launch()
+
+        image_bytes = await template_to_pic(
+            template_path=getcwd() + "/src/clover_html/fortune",
+            template_name="main.html",
+            templates={"data": data},
+            pages={
+                "viewport": {"width": 540, "height": 1},
+                "base_url": f"file://{getcwd()}",
+            },
+            wait=2,
+        )
+        await save_img(image_bytes,temp_file)
+        await browser.close()
+        await fortune_by_sqlite.send(MessageSegment.file_image(Path(temp_file)))
+        await delete_file(temp_file)
         await fortune_by_sqlite.finish(MessageSegment.keyboard(Keyboard_fortune))
-    except exception.ActionFailed as e:
-        print("\033[32m" + str(time.strftime("%m-%d %H:%M:%S")) +
-              "\033[0m [" + "\033[31;1mFAILED\033[0m" + "]" +
-              "\033[31;1m nonebot.adapters.qq.exception.ActionFailed \033[0m" + str(e))
-        await fortune_by_sqlite.finish("您的今日运势被外星人抢走啦，请重试。这绝对不是咱的错，绝对不是！")
+    except Exception as e:
+        if isinstance(e, FinishedException):
+            return
+        logger.error(f"获取运势失败：{e}")
+        await fortune_by_sqlite.finish("获取运势失败")
 
 
 tarot = on_command("今日塔罗", rule=to_me(), priority=10)
@@ -64,10 +88,10 @@ async def get_tarot(message: MessageEvent):
         try:
             await tarot.send(msg)
             await tarot.finish(MessageSegment.keyboard(Keyboard_fortune))
-        except exception.ActionFailed as e:
-            print("\033[32m" + str(time.strftime("%m-%d %H:%M:%S")) +
-                  "\033[0m [" + "\033[31;1mFAILED\033[0m" + "]" +
-                  "\033[31;1m nonebot.adapters.qq.exception.ActionFailed \033[0m" + str(e))
+        except Exception as e:
+            if isinstance(e, FinishedException):
+                return
+            logger.error(f"获取塔罗牌失败：{e}")
             await tarot.finish("您的塔罗牌被未来人抢走啦，请重试。这绝对不是咱的错，绝对不是！")
     else:
         await tarot.finish(MessageSegment.file_image(Path(result.image)))
