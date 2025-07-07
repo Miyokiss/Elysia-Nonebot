93c34b10170ba73ffb1718e1dd067ae9e487ff85
Miyokiss <Miyokiss@miyo.wiki>
2025-07-07 23:26:09 +0800

    feat(bili_vid_search): 优化视频发送逻辑
    - 增加了将视频上传到 Kukufile 并生成二维码的功能
    - 优化了错误处理和日志记录

--- src/clover_image/download_image.py
+++ src/clover_image/download_image.py
@@ -1,4 +1,8 @@
+import os
 import aiohttp
+from nonebot import logger
+
+__name__ = 'download_image'
 
 async def download_image(url,file_path):
     """
@@ -11,6 +15,8 @@ async def download_image(url,file_path):
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 response.raise_for_status()
+                if not os.path.exists(os.path.dirname(file_path)):
+                    os.makedirs(os.path.dirname(file_path))
                 with open(file_path, 'wb') as file:
                     while True:
                         chunk = await response.content.read(8192)
@@ -18,4 +24,4 @@ async def download_image(url,file_path):
                             break
                         file.write(chunk)
     except aiohttp.ClientError as e:
-        print(f"下载图片时出错: {e}")
+        logger.error(f"下载图片时出错: {e}")
