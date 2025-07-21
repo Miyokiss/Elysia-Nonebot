aa8a9ecd018124100aa0724facbe31be6afe0786
Miyokiss <Miyokiss@miyo.wiki>
2025-07-22 01:27:06 +0800

    refactor(config): 更新 fortune 键盘 ID

--- src/configs/Keyboard_config.py
+++ src/configs/Keyboard_config.py
@@ -1,6 +1,6 @@
 from nonebot.adapters.qq.message import MessageKeyboard
 
 Keyboard_ai = MessageKeyboard(id="102735560_1747882702")
-Keyboard_fortune = MessageKeyboard(id="102735560_1750144152")
+Keyboard_fortune = MessageKeyboard(id="102735560_1752897972")
 Keyboard_mate = MessageKeyboard(id="102735560_1751515676")
 Keyboard_valkyrie = MessageKeyboard(id="102735560_1750145279")
