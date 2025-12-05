d45de8bf6adc082670eba1a6bdc920efab961887
Miyokiss <Miyokiss@miyo.wiki>
2025-12-05 12:43:27 +0800

    ``` feat(bot): 添加定时重启功能
    - 引入 subprocess 模块以支持进程启动
    - 新增 reboot() 函数实现 Bot 重启逻辑
    - 配置调度器每两小时执行一次重启任务
    - 提供清理缓存和重启的日志记录
    ```

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import glob
 import threading
 import nonebot
 import logging.config
+import subprocess
 from pathlib import Path
 from nonebot import logger
 from nonebot.log import default_format
@@ -45,11 +46,18 @@ def clean_temp_cache():
             os.remove(file)
     logger.info("清理完成")
 
+def reboot():
+    """重启Bot"""
+    logger.info("开始重启...")
+    subprocess.Popen(["runtime/python", "Reboot.py"]) # 如果你使用了集成环境，请将python路径替换为集成环境路径
+
 def get_files_in_folder(folder_path: Path):
     return [Path(f) for f in glob.glob(str(folder_path / "*")) if Path(f).is_file()]
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(clean_temp_cache, 'cron', hour=0, minute=0)
+# 每隔两小时重启
+scheduler.add_job(reboot, 'interval', hours=2)
 
 if __name__ == "Bot":
     flask_thread = threading.Thread(target=start_flask, daemon=True)
