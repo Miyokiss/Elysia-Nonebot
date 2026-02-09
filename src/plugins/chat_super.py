fd1453879242028a4767de96a766416b47dd408c
Miyo <Miyokiss@miyo.wiki>
2026-02-09 19:24:43 +0800

    feat(chat_super): 优化群聊功能状态检查，添加管理员权限验证和未开放提示

--- src/plugins/chat_super.py
+++ src/plugins/chat_super.py
@@ -21,8 +21,14 @@ async def handle_function(message: MessageEvent):
         logger.debug("群聊环境")
         user_id, group_openid, content = message.get_user_id(), message.group_openid, message.get_plaintext().split()
         current_mode = await GroupChatRole.is_on(group_openid)
-        if current_mode == MODE_OFF:
+        if current_mode == MODE_ELYSIA:
+            pass
+        elif current_mode == MODE_OFF:
+             await Elysia_super.finish("未开放此功能。")
+        elif not await GroupChatRole.get_admin_list(group_openid, user_id):
             await Elysia_super.finish("未开放此功能。")
+        else:
+            pass
     else:
         logger.debug("私聊环境")
         user_id,content = message.get_user_id(), message.get_plaintext().split()
