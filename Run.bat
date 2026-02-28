63e0c52cbbf698e42c9535a61bd195567ca6f842
Miyo <Miyokiss@miyo.wiki>
2026-02-28 19:58:46 +0800

    feat: 更新Run.bat中的提示信息，明确当前使用的分支为ElysiaforMarkdown

--- Run.bat
+++ Run.bat
@@ -1,7 +1,7 @@
 @echo off
 @REM 提示
 color 4
-echo 当前使用为Elysia分支
+echo 当前使用为ElysiaforMarkdown分支
 set PATH=%PATH%;%cd%
 @REM 配置环境 如果你使用了集成环境，请将python.exe的路径改为你的python.exe的路径
 runtime\python.exe bot.py
